95affe598ccd9845167efdc59a55453c8f306954
Wiltort <wiltort21@gmail.com>
2023-09-25 21:18:29 +0500

    vrode sdelal

---
+++ js/sign.js
@@ -0,0 +1,7 @@
+function sign_in(){
+    let Name = prompt("Ваше имя: ");
+    let Number = prompt("Номер телефона:");
+    alert(
+      `Спасибо за регистрацию, ${Name}, наши консультанты свяжутся с вами по номеру ${Number}.`);
+  }
+
